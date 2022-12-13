910217c71191f7063d8721b8e5b8825ddea342d9
just-curious12 <manahil1078@hotmail.com>
2022-12-13 19:29:13 +0500

    added comments on the updatelex func

--- Indexer.py
+++ Indexer.py
@@ -33,6 +33,9 @@ class search():
     def storeInvertedIndex(self):
         with open('invertedIndex.json', 'w') as f:
             f.write(json.dumps(self.invertedIndex))
+    
+    #function takes in empty/already formed lexicon and frwrd indexed files, and updates it for the
+    #file included
 
     def updateLex(self, file):
         self.loadLex()
@@ -41,23 +44,32 @@ class search():
 
         with open(file) as f:
             data = json.load(f)
-        for i in data:
+        for i in data:      #loop over entries in file dictionary, where entries are articles
             pos = 0
             temp = i["title"].split()
-            for word in temp:
+            for word in temp:   #first, looping over words in the title of the article
                 if (word in stop_words or word in string.punctuation):
-                    pass
-                else:
+                    pass        #if stopwords, or punctuation, no op
+                else:           
                     if (ps.stem(word) not in self.lexicon.keys()):
+                        #if word stem is not in the lex file, then add it as key in lex dictionary 
+                        #and set value to pos in lex file
                         self.lexicon[ps.stem(word)] = str(len(self.lexicon))
+                        #now check if docID is in frwd indexed file
                     if (i["id"] in self.forwardIndex.keys()):
+                        #check if wordID present in frwd indexed file
                         if (self.lexicon[ps.stem(word)] in self.forwardIndex[i["id"]].keys()):
+                            #if present, then append pos of word to the list of pos
                             self.forwardIndex[i["id"]][self.lexicon[ps.stem(word)]].append(
                                 pos)
                         else:
+                            #if not present, then add a key of wordID and save a list with the pos 
+                            #as its value
                             self.forwardIndex[i["id"]][self.lexicon[ps.stem(word)]] = [
                                 pos]
                     else:
+                        #if docID not already present, add the docID and join wordId and
+                        #pos as a dict entry for its value
                         self.forwardIndex[i["id"]] = {
                             self.lexicon[ps.stem(word)]: [pos]}
 
